c3da317220067b9256d894baeb9ce5f60ed654c9
unknown <kulangana@yandex.ru git config --global user.name Kulangana-ddgit --global user.email kulangana@yandex.ru>
2022-11-13 20:25:50 +0300

    Добавлена программа решения

---
+++ task/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.*/
+
+string[] array = { "hello", "2", "world", ":-)", "1234", "-2", "computer science", "Russia" };
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}   ");
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine();
+
+string[] array2 = new string[count];
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        array2[j] = array[i];
+        Console.Write($"{array2[j]}   ");
+        j++;
+    }
+}
